97ea74b5b6b7da8fad991dc1c3258e61162d4618
= <yhy.kng@gmail.com>
2020-12-09 10:30:00 +0330

    Zibal multiplexing payment gateway for woocommerce first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: zibal payment gateway for woocommerce with multiplexing option
+Version: 1.5
+Description:  افزونه درگاه پرداخت زیبال برای فروشگاه ساز ووکامرس با قابلیت انجام تسهیم حین پرداخت
+Plugin URI: http://zibal.ir
+Author: Yahya Kangi
+Author URI: http://zibal.ir/
+
+*/
+include_once("class-wc-gateway-zibal.php");
